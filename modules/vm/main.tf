ddb63ef8489136b6317282eae1b068a06ccf6686
Debabrata Pain <debabrata.pain@cloud4c.com>
2026-05-10 22:14:40 +0530

    corrected variables to run tf plan

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -21,7 +21,7 @@ disable_password_authentication = true
 
 admin_ssh_key {
   username   = var.admin_username
-  public_key = var.public_key
+  public_key = var.public_key != null ? var.public_key : file("~/.ssh/id_rsa.pub")
   }
 
   network_interface_ids = [
