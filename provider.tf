88ffb03a59d0491897846fcfa77fdc594326dc3e
Debabrata Pain <debabrata.pain@cloud4c.com>
2026-05-12 16:02:53 +0530

    added multi environment terraform pipeline

--- provider.tf
+++ provider.tf
@@ -13,4 +13,6 @@ provider "azurerm" {
   features {}
   subscription_id = "f2671501-63ad-4140-b3b4-443e5c06ff86" # Your Azure subscription ID
   tenant_id       = "b24e2927-f763-4eda-b681-1f150c208e21" # Your tenant ID
+
+  use_cli = false
 }
