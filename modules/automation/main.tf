c700f922aec7007bc28aedccac8285ab789ac275
Debabrata Pain <debabrata.pain@cloud4c.com>
2026-05-14 13:02:19 +0530

    added automation runbook schedules

--- modules/automation/main.tf
+++ modules/automation/main.tf
@@ -46,3 +46,63 @@ resource "azurerm_automation_runbook" "stop_vm" {
 
   content = file("${path.root}/scripts/stop-vms.ps1")
 }
+
+resource "azurerm_automation_schedule" "start_schedule" {
+
+  name                    = "start-vm-schedule"
+
+  resource_group_name     = var.resource_group_name
+  automation_account_name = azurerm_automation_account.auto.name
+
+  frequency = "Day"
+  interval  = 1
+
+  timezone  = "Asia/Kolkata"
+
+  start_time = "2026-05-15T08:00:00+05:30"
+
+  description = "Start VMs every morning"
+}
+
+resource "azurerm_automation_schedule" "stop_schedule" {
+
+  name                    = "stop-vm-schedule"
+
+  resource_group_name     = var.resource_group_name
+  automation_account_name = azurerm_automation_account.auto.name
+
+  frequency = "Day"
+  interval  = 1
+
+  timezone  = "Asia/Kolkata"
+
+  start_time = "2026-05-15T20:00:00+05:30"
+
+  description = "Stop VMs every evening"
+}
+
+resource "azurerm_automation_job_schedule" "start_job" {
+
+  resource_group_name     = var.resource_group_name
+  automation_account_name = azurerm_automation_account.auto.name
+
+  schedule_name = azurerm_automation_schedule.start_schedule.name
+  runbook_name  = azurerm_automation_runbook.start_vm.name
+
+  parameters = {
+    ResourceGroupName = var.resource_group_name
+  }
+}
+
+resource "azurerm_automation_job_schedule" "stop_job" {
+
+  resource_group_name     = var.resource_group_name
+  automation_account_name = azurerm_automation_account.auto.name
+
+  schedule_name = azurerm_automation_schedule.stop_schedule.name
+  runbook_name  = azurerm_automation_runbook.stop_vm.name
+
+  parameters = {
+    ResourceGroupName = var.resource_group_name
+  }
+}
