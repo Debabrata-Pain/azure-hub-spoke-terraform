b2e138e47c72df74784c0a5158e783b8ded50c89
Debabrata Pain <debabrata.pain@cloud4c.com>
2026-05-14 12:44:43 +0530

    creating runbooks

--- modules/automation/main.tf
+++ modules/automation/main.tf
@@ -16,3 +16,33 @@ resource "azurerm_role_assignment" "automation_vm_contributor" {
 
   principal_id = azurerm_automation_account.auto.identity[0].principal_id
 }
+
+resource "azurerm_automation_runbook" "start_vm" {
+
+  name                    = "start-vms"
+  location                = var.location
+  resource_group_name     = var.resource_group_name
+  automation_account_name = azurerm_automation_account.auto.name
+
+  log_verbose = true
+  log_progress = true
+
+  runbook_type = "PowerShell"
+
+  content = file("${path.root}/scripts/start-vms.ps1")
+}
+
+resource "azurerm_automation_runbook" "stop_vm" {
+
+  name                    = "stop-vms"
+  location                = var.location
+  resource_group_name     = var.resource_group_name
+  automation_account_name = azurerm_automation_account.auto.name
+
+  log_verbose = true
+  log_progress = true
+
+  runbook_type = "PowerShell"
+
+  content = file("${path.root}/scripts/stop-vms.ps1")
+}
