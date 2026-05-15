94e173fea38e4c5da4d3b60a26275738803babc9
Debabrata Pain <debabrata.pain@cloud4c.com>
2026-05-15 15:56:02 +0530

    fixed checkov skip syntax

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,4 +1,4 @@
-#checkov:skip=CKV2_AZURE_31: NSG association handled externally via root module association resource  
+#checkov:skip=CKV2_AZURE_31:NSG association handled externally
 resource "azurerm_subnet" "subnet" {
   name                 = var.name
   resource_group_name  = var.resource_group_name
