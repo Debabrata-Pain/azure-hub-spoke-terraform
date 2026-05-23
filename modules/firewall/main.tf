a1f6bd7a25a1b1f05c02b69b0e59b6cb7af689b4
Debabrata Pain <debabrata.pain@cloud4c.com>
2026-05-23 13:27:16 +0530

    running pipeline correctly again 5.0

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -55,133 +55,39 @@ resource "azurerm_firewall" "fw" {
   }
 }
 
-resource "azurerm_firewall_network_rule_collection" "network_rules" {
-  name                = "network-rules"
-  azure_firewall_name = azurerm_firewall.fw.name
-  resource_group_name = var.resource_group_name
+  resource "azurerm_firewall_policy_rule_collection_group" "fw_rules" {
 
-  priority = 100
-  action   = "Allow"
+    name               = "fw-rule-group"
+    firewall_policy_id = azurerm_firewall_policy.fw_policy.id
 
-  # Inter-spoke communication
-  rule {
-    name = "allow-interspoke"
-
-    source_addresses = [
-      "10.1.0.0/16",
-      "10.2.0.0/16"
-    ]
-
-    destination_addresses = [
-      "10.1.0.0/16",
-      "10.2.0.0/16"
-    ]
-
-    destination_ports = ["*"]
-
-    protocols = ["Any"]
-  }
-
-  # SSH access
-  rule {
-    name = "allow-ssh"
-
-    source_addresses = [
-      var.admin_public_ip
-    ]
-
-    destination_addresses = [
-      "10.1.0.0/16",
-      "10.2.0.0/16"
-    ]
-
-    destination_ports = ["22"]
-
-    protocols = ["TCP"]
-  }
-
-  rule {
-  name = "allow-internet"
-
-  source_addresses = [
-    "10.1.0.0/16",
-    "10.2.0.0/16"
-  ]
-
-  destination_addresses = [
-    "*"
-  ]
-
-  destination_ports = [
-    "*"
-  ]
-
-  protocols = [
-    "Any"
-  ]
-}
-
-}
-
-resource "azurerm_firewall_application_rule_collection" "app_rules" {
-  name                = "application-rules"
-  azure_firewall_name = azurerm_firewall.fw.name
-  resource_group_name = var.resource_group_name
-
-  priority = 200
-  action   = "Allow"
-
-  rule {
-    name = "allow-web"
-
-    source_addresses = [
-      "10.1.0.0/16",
-      "10.2.0.0/16"
-    ]
-
-    target_fqdns = ["*"]
-
-    protocol {
-      port = 80
-      type = "Http"
-    }
-
-    protocol {
-      port = 443
-      type = "Https"
-    }
-  }
-}
-
-resource "azurerm_firewall_policy_rule_collection_group" "fw_rules" {
-
-  name               = "fw-rule-group"
-  firewall_policy_id = azurerm_firewall_policy.fw_policy.id
-
-  priority = 100
-
-  nat_rule_collection {
-    name     = "dnat-rules"
     priority = 100
-    action   = "Dnat"
 
-    # APP1 VM
-    rule {
-      name = "app1-ssh"
+  # ==================================================
+  # NAT RULE COLLECTION
+  # ==================================================
 
-      protocols = ["TCP"]
+    nat_rule_collection {
+      name     = "dnat-rules"
+      priority = 100
+      action   = "Dnat"
 
-      source_addresses = [
-        var.admin_public_ip
-      ]
+      # APP1 VM
+      rule {
+        name = "app1-ssh"
 
-      destination_address = azurerm_public_ip.fw_pip.ip_address
+        protocols = ["TCP"]
 
-      destination_ports = ["2221"]
+        source_addresses = [
+          var.admin_public_ip
+        ]
 
-      translated_address = var.app1_private_ip
-      translated_port    = "22"
-    }
+        destination_address = azurerm_public_ip.fw_pip.ip_address
+
+        destination_ports = ["2221"]
+
+        translated_address = var.app1_private_ip
+        translated_port    = "22"
+      }
 
     # APP2 VM
     rule {
@@ -235,6 +141,103 @@ resource "azurerm_firewall_policy_rule_collection_group" "fw_rules" {
 
       translated_address = var.db2_private_ip
       translated_port    = "22"
+    }
+  }
+
+# ==================================================
+  # NETWORK RULE COLLECTION
+  # ==================================================
+
+ network_rule_collection {
+    name     = "network-rule"
+    priority = 200
+    action   = "Allow"
+
+    # Inter-spoke communication
+    rule {
+      name = "allow-interspoke"
+
+      protocols = ["Any"]
+
+      source_addresses = [
+        "10.1.0.0/16",
+        "10.2.0.0/16"
+      ]
+
+      destination_addresses = [
+        "10.1.0.0/16",
+        "10.2.0.0/16"
+      ]
+
+      destination_ports = ["*"]
+    }
+
+    # SSH access
+    rule {
+      name = "allow-ssh"
+
+      protocols = ["TCP"]
+
+      source_addresses = [
+        var.admin_public_ip
+      ]
+
+      destination_addresses = [
+        "10.1.0.0/16",
+        "10.2.0.0/16"
+      ]
+
+      destination_ports = ["22"]
+    }
+
+    # Internet access
+    rule {
+      name = "allow-internet"
+
+      protocols = ["Any"]
+
+      source_addresses = [
+        "10.1.0.0/16",
+        "10.2.0.0/16"
+      ]
+
+      destination_addresses = [
+        "*"
+      ]
+
+      destination_ports = ["*"]
+    }
+  }
+
+
+# ==================================================
+  # APPLICATION RULE COLLECTION
+  # ==================================================
+
+  application_rule_collection {
+    name     = "app-rule"
+    priority = 300
+    action   = "Allow"
+
+    rule {
+      name = "allow-web"
+
+      source_addresses = [
+        "10.1.0.0/16",
+        "10.2.0.0/16"
+      ]
+
+      destination_fqdns = ["*"]
+
+      protocols {
+        port = 80
+        type = "Http"
+      }
+
+      protocols {
+        port = 443
+        type = "Https"
+      }
     }
   }
 }
